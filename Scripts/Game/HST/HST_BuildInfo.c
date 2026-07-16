8d538064a4ec049a34172bd688f8bb992c9312dc
Austin Baker <noise.9no@gmail.com>
2026-07-16 08:17:41 -0400

    Restamp prepared settlement restart proof

--- Scripts/Game/HST/HST_BuildInfo.c
+++ Scripts/Game/HST/HST_BuildInfo.c
@@ -1,7 +1,7 @@
 class HST_BuildInfo
 {
-	static const string BUILD_SHA = "db91cb680685fd79019b735fe6d56efc9825a1d3";
-	static const string BUILD_UTC = "2026-07-16T12:04:47Z";
+	static const string BUILD_SHA = "02f64410670a3ffced10c8e099c05eaf5a469cb0";
+	static const string BUILD_UTC = "2026-07-16T12:17:23Z";
 	static const string BUILD_LABEL = "schema70-settings24-counterattack-prepared-settlement-restart-proof";
 
 	static string BuildSummary()
